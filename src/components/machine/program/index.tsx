0626129b117ea6ba17cdc90e0c01feb0d629da1d
Gabrielle B Zhu <nightbelle1130@gmail.com>
2023-10-12 22:05:28 +1100

    Create basic structure for tm program

---
+++ src/components/machine/program/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+const ProgramComponent: React.FC = ()=> {
+  return (
+    <div className="tape-container">
+      <div>Program</div>
+      <div className="program-input__container">
+        <textarea />
+      </div>
+    </div>
+  );
+}
+
+export default ProgramComponent;
