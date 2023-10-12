27dd4d0beea31b357523b991e6d92796d1c95de9
Gabrielle B Zhu <nightbelle1130@gmail.com>
2023-10-12 22:30:02 +1100

    add buttons for program

--- src/components/machine/program/index.tsx
+++ src/components/machine/program/index.tsx
@@ -1,10 +1,19 @@
 import * as React from "react";
+import useProgramHooks from "./program-hooks";
 
 const ProgramComponent: React.FC = ()=> {
+  const {handleStart, handleStop} = useProgramHooks();
+
   return (
     <div className="tape-container">
       <div>Program</div>
       <div className="program-input__container">
+        <div className="program-input__controller">
+          <button onClick={handleStart}>Start</button>
+          <button onClick={handleStop}>Pause</button>
+          <button>Step</button>
+          <button>Reset</button>
+        </div>
         <textarea />
       </div>
     </div>
