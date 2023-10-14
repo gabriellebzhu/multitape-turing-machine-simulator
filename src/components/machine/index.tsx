3c28a119fafff58f0643fae2a9032bd017e93969
Gabrielle B Zhu <nightbelle1130@gmail.com>
2023-10-14 17:54:32 +1100

    Add state displayer

--- src/components/machine/index.tsx
+++ src/components/machine/index.tsx
@@ -1,15 +1,38 @@
 import * as React from "react";
 import TapesComponent from "./tape";
 import ProgramComponent from "./program";
+import StateComponent from "./state";
 
 const Simulator: React.FC = () => {
+  const { tapes, handleAddTape, handleRemoveTape } = useTapes();
+  const machineStates = useMachineStates();
+  const { steps, setSteps, isRunning, setIsRunning } = machineStates;
+  const { tm, machineState, handleStart, handleStop } = useProgram({
+    tapes,
+    setIsRunning,
+    setSteps,
+  });
   return (
     <div>
       <h1>Multitape Turing Simulator</h1>
-      <TapesComponent />
-      <ProgramComponent />
+      <TapesComponent
+        tapes={tapes}
+        handleAddTape={handleAddTape}
+        handleRemoveTape={handleRemoveTape}
+        isRunning={isRunning}
+      />
+      <StateComponent
+        state={machineState}
+        isRunning={isRunning}
+        steps={steps}
+      />
+      <ProgramComponent
+        handleStart={handleStart}
+        handleStop={handleStop}
+        isRunning={isRunning}
+      />
     </div>
   );
-}
+};
 
 export default Simulator;
