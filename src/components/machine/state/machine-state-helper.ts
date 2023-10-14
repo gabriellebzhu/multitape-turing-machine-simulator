81faa5901f42e1deeb819ebbd4ee365b66e6b971
Gabrielle B Zhu <nightbelle1130@gmail.com>
2023-10-14 18:46:13 +1100

    fix `isStopState`

--- src/components/machine/state/machine-state-helper.ts
+++ src/components/machine/state/machine-state-helper.ts
@@ -3,5 +3,5 @@ export const SPECIAL_STATES = ["halt-accept", "halt-reject", "halt"] as const;
 export type SpecialState = (typeof SPECIAL_STATES)[number];
 
 export const isStopState = (state: string) => {
-  return SPECIAL_STATES.find((s) => s === state) !== null;
+  return !!SPECIAL_STATES.find((s) => s === state);
 };
