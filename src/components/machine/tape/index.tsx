af72b33ce0b8c52bb30e7f49e38aa1c0ed8dde1a
Gabrielle B Zhu <nightbelle1130@gmail.com>
2023-10-14 17:56:39 +1100

    update tape displayer with disable button

--- src/components/machine/tape/index.tsx
+++ src/components/machine/tape/index.tsx
@@ -1,22 +1,43 @@
 import * as React from "react";
-import { Tape } from "./tape";
 import { TapeDisplayer } from "./tape-displayer";
-import useTapeHooks from "./tape-hooks";
+import { Tape } from "./tape";
 
-const TapesComponent: React.FC = ()=> {
-  const {tapes, handleAddTape, handleRemoveTape} = useTapeHooks();
+interface TapesComponentProps {
+  tapes: Array<Tape>;
+  handleAddTape: () => void;
+  handleRemoveTape: (index: number) => void;
+  isRunning: boolean;
+}
+
+const TapesComponent: React.FC<TapesComponentProps> = (
+  props: TapesComponentProps
+) => {
+  const { tapes, handleAddTape, handleRemoveTape, isRunning } = props;
 
   return (
-      <div className="tapes-container">
-          <div>Tapes</div>
-          <div className="tapes-display__container">
-            {tapes.map((t, i) => <TapeDisplayer handleRemoveTape={handleRemoveTape} tape={t} index={i}/>)}
-          </div>
-          <div className="add-tape__container">
-            <button className="add-tape_btn" onClick={handleAddTape}>Add tape</button>
-          </div>
+    <div className="tapes-container">
+      <div>Tapes</div>
+      <div className="tapes-display__container">
+        {tapes.map((t, i) => (
+          <TapeDisplayer
+            handleRemoveTape={handleRemoveTape}
+            tape={t}
+            index={i}
+            isDisabled={isRunning}
+          />
+        ))}
       </div>
+      <div className="add-tape__container">
+        <button
+          className="add-tape_btn"
+          disabled={isRunning}
+          onClick={handleAddTape}
+        >
+          Add tape
+        </button>
+      </div>
+    </div>
   );
-}
+};
 
 export default TapesComponent;
