08c5b91bb99485d8d6598ef5b814f4c9bf7d4f4c
Gabrielle B Zhu <77947405+gabriellebzhu@users.noreply.github.com>
2024-02-23 14:02:09 +1100

    Setup Jasmine as unit testing lib

---
+++ webpack-test.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const glob = require("glob");
+
+module.exports = {
+  entry: glob.sync("./src/**/*test.ts?(x)"),
+  output: {
+    filename: "test.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/, // styles files
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
